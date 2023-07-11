bcbee05490050558f9730ae674846e7852f9e654
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-07-11 20:37:38 +0200

    updated to .8.19

--- src/interfaces/IUniswapV2ERC20.sol
+++ src/interfaces/IUniswapV2ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
 interface IUniswapV2ERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
